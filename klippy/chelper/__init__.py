dmbutyugin/klipper

extruder: Added support for time offset of extruder vs kinematic moves
Signed-off-by: Dmitry Butyugin <dmbutyugin@google.com>

klippy/chelper/__init__.py
@@ -141,7 +141,7 @@ defs_kin_extruder = """
     struct stepper_kinematics *extruder_stepper_alloc(void);
     void extruder_set_pressure_advance(struct stepper_kinematics *sk
         , double linear_velocity, double linear_offset
-        , double linear_advance, double smooth_time);
+        , double linear_advance, double smooth_time, double offset_time);
 """
 
 defs_kin_shaper = """

dprintingiscoo8/danger-klipper-peziod

klippy/chelper/__init__.py
@@ -143,7 +143,7 @@ defs_kin_winch = """
 defs_kin_extruder = """
     struct stepper_kinematics *extruder_stepper_alloc(void);
     void extruder_set_pressure_advance(struct stepper_kinematics *sk
-        , double pressure_advance, double smooth_time);
+        , double pressure_advance, double smooth_time, double time_offset);
 """
 
 defs_kin_shaper = """